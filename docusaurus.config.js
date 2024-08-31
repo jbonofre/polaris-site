13dd02325a9f6651499e204f441458ec85bd2493
JB Onofré <jbonofre@apache.org>
2024-08-31 07:32:59 +0200

    Fix docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'apache', // Usually your GitHub org/user name.
-  projectName: 'polaris-site', // Usually your repo name.
+  organizationName: 'apache/polaris-site', // Usually your GitHub org/user name.
+  projectName: 'apache/polaris-site', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
